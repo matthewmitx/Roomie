2d3bc67f2fd9b6a29c7a8ebdc45593e7a451243b
Matthew Mitchell <matthewmitchell@umail.ucsb.edu>
2018-07-02 14:25:06 -0700

    Basic creation of empty xcode application

---
+++ Roomie/Roomie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roomie
+//
+//  Created by Matthew Mitchell on 7/2/18.
+//  Copyright © 2018 Matt Mitch Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
